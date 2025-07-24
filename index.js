635d8bd7e852fb0baf200943809e1a5b1e4952dd
Carlos Canet <carlos@carlosca.net>
2025-07-24 09:55:59 +0200

    Done yesterday :(

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
+const str = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`;
 
 
 // Print out the concatenated string
-
-
+console.log(str);
 
 
 /*******************************************
@@ -22,10 +22,12 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+let result = part1.slice(0, part1.length-1) + part1[part1.length - 1].toUpperCase();
+result += part2.slice(0, part2.length-1) + part2[part2.length - 1].toUpperCase();
 
 
 // Print the cameLtaiL-formatted string
-
+console.log(result);
 
 
 
@@ -35,10 +37,10 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const tipAmount = billTotal * 0.15;
 
 // Print out the tipAmount
-
+console.log(tipAmount);
 
 
 
@@ -47,11 +49,16 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
+function getRandomIntInclusive(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+}
 
+const randomNumber = getRandomIntInclusive(1, 10);
 
 // Print the generated random number
-
-
+console.log(randomNumber);
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -62,15 +69,30 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+// FALSE
 
 const expression2 = a || b;
+// TRUE
 
 const expression3 = !a && b;
+// FALSE
 
 const expression4 = !(a && b);
+// TRUE
 
 const expression5 = !a || !b;
+// TRUE
 
 const expression6 = !(a || b);
+// FALSE
 
 const expression7 = a && a;
+// TRUE
+
+// console.log(expression1);
+// console.log(expression2);
+// console.log(expression3);
+// console.log(expression4);
+// console.log(expression5);
+// console.log(expression6);
+// console.log(expression7);
